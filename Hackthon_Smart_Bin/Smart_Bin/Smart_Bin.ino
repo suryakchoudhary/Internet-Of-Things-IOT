271df1f694065fadf7fc24379d0c7e9f22596e17
suryakchoudhary <surya333kant@gmail.com>
2022-02-12 16:58:30 +0530

    Hackthon Modified Code

--- Hackthon_Smart_Bin/Smart_Bin/Smart_Bin.ino
+++ Hackthon_Smart_Bin/Smart_Bin/Smart_Bin.ino
@@ -3,6 +3,10 @@
 #include "DHT.h"        //include library for DHT sensor
 #include <ThingESP.h>
 
+ThingESP32 thing("suryakant", "SmartBin", "ESP32cp-2102");
+
+RTC_DATA_ATTR int bootCount = 0;  // Saving data to Real Time Clock
+
 // ULTRASONIC
 #define TRIG 4
 #define RECEIV 2
@@ -10,7 +14,7 @@
 // DHT11_HUMIDITY_TEMPREATURE
 #define DHTPIN 15 // DHT11 Data pin connected to esp32 pin no.
 
-#define RED 5
+#define RED 21
 #define GREEN 18
 #define BLUE 19
 
@@ -20,8 +24,8 @@
 //#define MAX 10
 
 // WIFI CREDENTIALS
-char ssid[] = "Ferrari";   // your network SSID (name) 
-char pass[] = "pawan@@0433kant";
+/*char ssid[] = "Ferrari";   // your network SSID (name) 
+char pass[] = "pawan@@0433kant";*/
 WiFiClient  client;
 
 DHT dht(DHTPIN, DHT11);
@@ -29,9 +33,8 @@ DHT dht(DHTPIN, DHT11);
 unsigned long myChannelNumber = 1652835;           //channal id from thingspeak
 const char * myWriteAPIKey = "CGQMNB4RTI07RHQC";   // write api kay from thingspeak
 const int Fill_Level_Field1 =1;                    //channel field 1 for Level
-const int Temp_Field2 =2;                          //channel field 1 for Temp.
-const int Humidity_Field3 = 3;                     //channel field 1 for Humidity
-
+const int Temp_Field2 =2;                          //channel field 2 for Temp.
+const int Humidity_Field3 = 3;                     //channel field 3 for Humidity
 
 
 void setup() 
@@ -45,16 +48,10 @@ void setup()
   pinMode(GREEN, OUTPUT);
   pinMode(BLUE, OUTPUT);
   dht.begin();
-  WiFi.mode(WIFI_STA);
+  //WiFi.mode(WIFI_STA);
   ThingSpeak.begin(client);  // Initialize ThingSpeak
 
-}
-
-void loop() 
-{
-  // put your main code here, to run repeatedly:
-  
-  if(WiFi.status() != WL_CONNECTED)
+ /* if(WiFi.status() != WL_CONNECTED)
   {
     Serial.print("Attempting to connect to SSID: ");
     Serial.println(ssid);
@@ -66,20 +63,52 @@ void loop()
       delay(2500);                             //THIS IS THE MINIMUM TIME REQUIRED TO CONNECT TO WI-FI
     } 
   }
+  Serial.println("\nConnected.");*/
+
+  thing.SetWiFi("Ferrari", "pawan@@0433kant");
+  //thing.SetWiFi("iQOO", "surya00000kant12");
   Serial.println("\nConnected.");
+  thing.initDevice();
+}
+
+// ThingESP: Follows MQTT protocol and acts as mediator to connect Twilio with ESP32 0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+String HandleReaponse(String query) 
+{
+  if (query == "led on") {
+    //digitalWrite(LED, 0);
+    return "Done: LED Turned ON";
+  }
+  /*if(query == "HCSRO4 STATUS")
+  return digitalRead(TRIG) ? "HCSR04 IS OFF" : "HCSR04 IS ON";
+
+  else return "INVALID QUERY";*/
+}
+                                                                                       //00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+
+void loop() 
+{
+  // put your main code here, to run repeatedly:
 
   float temperature;   // Variable to store temperature
   float humidity;      // Variable to store Humidity
   int status = 0;
+  int per = 0;     // Displaying fill level percent
 
   temperature = dht.readTemperature();    //read temperature
   humidity = dht.readHumidity();          //read Humidity
 
   // PRINTING TEMPREATURE
+  Serial.println("");
+  Serial.println("");
   Serial.print("TEMPREATURE: ");
   Serial.print(temperature);
   Serial.println("");
   Serial.println("");
+  if(temperature >= 25)
+  {
+    thing.sendMsg("+919773680338", "temperature is greater than 20 degree") ;
+    //delay(3000);
+  }
 
 
   // PRINTING HUMIDITY
@@ -90,9 +119,10 @@ void loop()
 
 
   // ULTRASONIC
-  int distance = 0, height = 0; 
+  unsigned int distance = 0;
+  int height = 0; 
   long duration;
-  static int count = 0;
+  //static int count = 0;
 
   // MEASURING DISTANCE(ULTRASONIC_)
   digitalWrite(TRIG, LOW);
@@ -103,76 +133,94 @@ void loop()
   duration = pulseIn(RECEIV, HIGH);
   distance = (duration * 0.034)/2;
 
-  height = (40 - distance); // 40 is the hright of the dustbin
+  height = (30 - distance); // 30 is the height of the dustbin
 
   // FILL LEVEL
 
   // EMPTY
-  if(distance >= 30)//EMPTY)
+  if(height <= 10)                                 //EMPTY
   {
     digitalWrite(GREEN, HIGH);
+    delay(2000);
     digitalWrite(BLUE, LOW);
     digitalWrite(RED, LOW);
     //status = digitalRead(GREEN);
-    Serial.print("EMPTY GREEN: ");
-    Serial.print(distance);
-    Serial.println("");
+    Serial.print("DUSTBIN IS EMPTY (GREEN_LED): ");
+    Serial.print(height);
+    Serial.println("cm");
     Serial.println("");
   }
 
   //HALF
-  else if(30 >= distance && distance > 20) //(MEDIUM < distance <= EMPTY)
+  else if(10 < height && height <= 20)            // HALF
   {
     digitalWrite(BLUE, HIGH);
+    delay(2000);
     digitalWrite(GREEN, LOW);
     digitalWrite(RED, LOW);
     //status = digitalRead(BLUE);
-    Serial.print("MEDIUM BLUE: ");
-    Serial.print(distance);
-    Serial.println("");
+    Serial.print("DUSTBIN IS HALF FILLED (BLUE_LED): ");
+    Serial.print(height);
+    Serial.println("cm");
     Serial.println("");
   }
 
   //FULL
-  else if(distance <= 20)//MEDIUM)
+  else if(height > 20)                              // FULL
   {
     digitalWrite(RED, HIGH);
+    delay(2000);
     digitalWrite(BLUE, LOW);
     digitalWrite(GREEN, LOW);
     //status = digitalRead(RED);
-    Serial.print("FILLED RED: ");
-    Serial.print(distance);
-    Serial.println("");
+    Serial.print("DUSTBIN IS FULL (RED_LED): ");
+    Serial.print(height);
+    Serial.println("cm");
+    thing.sendMsg("+919773680338", "Dustbin Fill Level Is Greather Than 66%") ;
+    //delay(3000);
     Serial.println("");
   }
+
+  // Percentage filled
+  per = (height * 0.034) * 100;                    // 1/30 = 0.025        30cm is the height of dustbin
+  Serial.print(per);
+  Serial.print("% FILLED");
+  Serial.println("");
+
+  // THRESHOLD ALERT ON WHATSAPP  wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
+/*  
+  if(height > 20)
+  {
+    
+  }*/
+  thing.Handle();               //////////////////////////////
+  
   
   Serial.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
 
-  // DEEP SLEEP MODE
-  if(distance >= 20)
-  {
-      esp_sleep_enable_timer_wakeup(2000);
-      delay(1000);
-      Serial.println("Sleep for 2 seconds");
-      esp_deep_sleep_start();
-      Serial.println("This will never be printed");  
-  }
-  else if(distance < 20 && distance >30)
-  {
-    esp_sleep_enable_timer_wakeup(3000);
-    Serial.println("Sleep for 3 seconds");
-    esp_deep_sleep_start();
-  }
-  else if(distance < 30)
-  {
-    esp_sleep_enable_timer_wakeup(4000);
-    Serial.println("")
-  }
-
-  delay(500);
-
-  ThingSpeak.writeField(myChannelNumber, Fill_Level_Field1, height, myWriteAPIKey);
+  ThingSpeak.writeField(myChannelNumber, Fill_Level_Field1, per, myWriteAPIKey);
   ThingSpeak.writeField(myChannelNumber, Temp_Field2, temperature, myWriteAPIKey);
   ThingSpeak.writeField(myChannelNumber, Humidity_Field3, humidity, myWriteAPIKey);
 
+  Serial.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+
+  // DEEP SLEEP MODE
+  if(height > 20)                                    // FILLED
+  {
+    esp_sleep_enable_timer_wakeup(2 * 1000000);   // Microsecond to second conversion factor
+    Serial.println("Sleep for 2 seconds");
+    esp_deep_sleep_start();  
+  }
+  else if(10 < height && height <= 20)                 //MEDIUM FILLED
+  {
+    esp_sleep_enable_timer_wakeup(4 * 1000000);   // Microsecond to second conversion factor
+    Serial.println("Sleep for 4 seconds");
+    esp_deep_sleep_start();
+  }
+  else if(height <= 10)                                  // EMPTY
+  {
+    esp_sleep_enable_timer_wakeup(8 * 1000000);    // Microsecond to second conversion factor
+    Serial.println("Sleep for 8 seconds");
+    esp_deep_sleep_start();
+  }
 }
